f2cfdef639a5898bb18aa96f161d2b9643a3f7fd
Rebecca Hazell <rhazell@companieshouse.gov.uk>
2020-12-03 09:49:49 +0000

    Branching added to 'is the applicant still acting' page
    yes - replacement address
    no - skip page and go to contact address

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,19 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Branching
+router.post('/service/applicant-acting-answer', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  const applicantacting = req.session.data['applicant-acting']
+
+  if (applicantacting === 'false') {
+    res.redirect('contact-address')
+  } else {
+    res.redirect('replacement-address')
+  }
+})
+
 module.exports = router
