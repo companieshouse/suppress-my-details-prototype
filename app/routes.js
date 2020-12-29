3ede164efe3d9a9ad15897dd9205f59b58d398f8
Rebecca Hazell <rhazell@companieshouse.gov.uk>
2020-12-29 13:03:52 +0000

    Starting to add validation

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,12 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.get('/', function (req, res) {
+  req.session.destroy()
+  res.render('index', {
+  })
+})
+
 // Branching
 router.post('/service/applicant-acting-answer', function (req, res) {
   // Get the answer from session data
@@ -15,6 +21,25 @@ router.post('/service/applicant-acting-answer', function (req, res) {
     res.redirect('contact-address')
   } else {
     res.redirect('replacement-address')
+  }
+})
+
+// Applicant-details
+
+router.post('/service/applicant-details', function (req, res) {
+  var errors = []
+  var str = req.session.data['full-name']
+  if (str === '') {
+    errors.push({
+      text: 'Enter full name',
+      href: '#full-name'
+    })
+    res.render('service/applicant-details', {
+      errorName: true,
+      errorList: errors
+    })
+  } else {
+    res.redirect('document-details')
   }
 })
 
